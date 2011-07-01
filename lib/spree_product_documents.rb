f97606c850d1265d639321c34a2b055825c12d1b
Valentino Stoll <valentino@mosaicwebsite.com>
2011-07-01 13:23:04 -0400

    Fixed naming convention issue

--- lib/spree_product_documents.rb
+++ lib/spree_product_documents.rb
@@ -1,26 +1,21 @@
 require 'spree_core'
 require 'spree_product_documents_hooks'
 
-module ProductDocuments
+module SpreeProductDocuments
   class Engine < Rails::Engine
-    config.autoload_paths += %W(#{config.root}/lib)
-  
     def self.activate
-    
-      # Add "Document" to Product Model
-      Product.class_eval do
-        has_many :docs, :as => :viewable, :order => :position, :dependent => :destroy
-      end
 
       # Make the Doc Helper available to all views
       Spree::BaseController.class_eval do
-	      helper DocsHelper
+        helper DocsHelper
       end
-    
+      
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.env == "production" ? require(c) : load(c)
       end
-
+      
     end
+    config.to_prepare &method(:activate).to_proc
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
